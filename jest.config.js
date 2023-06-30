4646055dfc652388c896920fcdf21c63a136be02
Arash Khajelou <a.khajelou@gmail.com>
2023-06-30 18:37:29 +0330

    Add the test and lint workflow.

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    testEnvironment: 'node',
+    moduleFileExtensions: ['js', 'json', 'vue'],
+    transform: {
+        '^.+\\.vue$': 'vue-jest',
+        '^.+\\.js$': 'babel-jest',
+    },
+}
